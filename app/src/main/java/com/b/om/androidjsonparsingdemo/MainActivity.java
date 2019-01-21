25c3ed08dacab47855a457a05ba7d1a0cca2d8f2
goradbhavin <goradbhavin25@gmail.com>
2019-01-21 13:50:47 +0530

    jsonparsing

--- app/src/main/java/com/b/om/androidjsonparsingdemo/MainActivity.java
+++ app/src/main/java/com/b/om/androidjsonparsingdemo/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     public void process(View view){
 
         if (view.getId() == R.id.btnFirstDemo){
